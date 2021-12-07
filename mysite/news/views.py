c2eba43cbc8a4981489af99d0cff2587f12058c0
hhhhpark <ddangle425@naver.com>
2021-12-07 14:58:48 +0900

    키워드 검색기능

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import NewsSearchForm
 
 
 @login_required(login_url="common:login")
@@ -15,4 +16,16 @@ def news_search(request):
     """
     뉴스 검색 화면을 반환한다.
     """
-    return render(request, "news/news_search.html", {},)
+
+    if request.method == "GET":
+        if len(request.GET):
+            form = NewsSearchForm(request.GET)
+
+            if form.is_valid():
+                pass
+        # 뉴스를 검색하지 않고 검색 페이지를 오픈할 때는
+        # form 유효성 체크를 하지 않도록 한다.
+        else:
+            form = NewsSearchForm()
+
+    return render(request, "news/news_search.html", {"form": form},)
